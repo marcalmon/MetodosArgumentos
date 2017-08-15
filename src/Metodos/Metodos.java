08e1dd489907593306f548198f9a0cf1128315a6
Mar Calmon <marical2222@gmail.com>
2017-08-15 15:37:11 -0300

    Martes 15 de agosto, metodos

--- src/Metodos/Metodos.java
+++ src/Metodos/Metodos.java
@@ -11,9 +11,36 @@ package Metodos;
  */
 public class Metodos {
     
+    //atributos de la clase Metodos
+    int edad=18;
+    String nombre="Juan";
+    String direccion="Av Chiclana 3444";
+    
+    public void ImprimirEdad(){//Metodo para imprimir edad
+    
+        System.out.println(edad);
+    }
+    
+    public void ImprimirNombre(){//Metodo para imprimir nombre
+    
+        System.out.println(nombre);
+    }
+    
+    public void ImprimirDireccion(){//Metodo para imprimir nombre
+    
+        System.out.println(direccion);
+    }
+    
     public static void main(String[] args){
     
-    System.out.println("HOLA MUNDO");
+       
+     Metodos metodo1= new Metodos();
+    
+    metodo1.ImprimirEdad();
+    metodo1.ImprimirNombre();
+    metodo1.ImprimirDireccion();
+    
+    
     }
     
 }
